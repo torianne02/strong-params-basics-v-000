bfe9a8005f6214e46da51c762e8c00d853d661cb
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-10 18:44:24 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,14 +12,14 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(post_params(:title, :description))
+	  @post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 	 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(post_params(:title))
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 	 
