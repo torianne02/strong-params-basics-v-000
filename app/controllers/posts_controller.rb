fee1105e4b1dbba9ea6c398f92f6d75d4cf051d6
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-10 18:37:53 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,12 +24,7 @@ class PostsController < ApplicationController
 	end
 	 
 	private
-	 
-	 
-	# We pass the permitted fields in as *args;
-	# this keeps `post_params` pretty dry while
-	# still allowing slightly different behavior
-	# depending on the controller action
+
 	def post_params(*args)
 	  params.require(:post).permit(*args)
 	end
